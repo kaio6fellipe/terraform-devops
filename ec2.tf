7697b8bdf1ac8c0f67b9ee9db07039a9c8232893
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-07-11 02:52:53 -0300

    Update no dia 11/07/2022 - 02:52:48

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ansible-server01" {
+  ami = "ami-0070c5311b7677678"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+  tags = {
+    Name = "ansible-server01"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = [aws_key_pair.terraform-aws]
+}
--- remote-state.tf
+++ remote-state.tf
@@ -1,9 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state"
-#     key = "terraform/terraform.tfstate"
-#     region = "us-east-1"
-#     encrypt = true
-# 
-#   }
-# }
+terraform {
+    backend "s3" {
+        bucket          = "terraform-state20220711040913053000000001"
+        key             = "state-devops/terraform.tfstate"
+        region          = "us-east-1"
+        encrypt         = true
+        kms_key_id      = "alias/terraform-bucket-key"
+        dynamodb_table  = "terraform-state-devops"
+    }
+}
