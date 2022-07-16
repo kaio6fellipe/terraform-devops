0dab6e475cf7bc12c00158d24543f4019c006d80
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-07-16 03:56:22 -0300

    Update no dia 16/07/2022 - 03:56:17

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "us-east-1" {
   }
 }
 
-module "sa-east-1" {
-  source = "./modules/multi-region"
-  instance_type = "${var.instance_type}"
-  ansible_instance_type = "${var.ansible_instance_type}"
-  environment = "${var.environment}"
-  providers = {
-    aws = aws.sae1
-  }
-}
+# module "sa-east-1" {
+#   source = "./modules/multi-region"
+#   instance_type = "${var.instance_type}"
+#   ansible_instance_type = "${var.ansible_instance_type}"
+#   environment = "${var.environment}"
+#   providers = {
+#     aws = aws.sae1
+#   }
+# }
---
+++ modules/multi-region/vpc.tf
@@ -0,0 +1,5 @@
+# Definir CIDR block da VPC entre /16 e /28
+# O CIDR block nao pode ser igual, cada VPC tem que ter uma
+# - prod = 10.99.0.0/16
+# - staging = 10.2.0.0/16
+# - dev = 10.1.0.0/16
