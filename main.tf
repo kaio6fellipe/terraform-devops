1425dc38323b46e6e81627c2831816f2061add5d
Kaio Fellipe <kaiofellipe_@hotmail.com>
2022-07-27 00:04:34 -0300

    Revert "Ajuste de layout dos repositorios"

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "us-east-1" {
-  source = "./multi-region/"
+  source = "./modules/multi-region"
   instance_type = "${var.instance_type}"
   ansible_instance_type = "${var.ansible_instance_type}"
   environment = "${var.environment}"
@@ -17,15 +17,3 @@ module "us-east-1" {
 #     aws = aws.sae1
 #   }
 # }
-
-module "apps" {
-  source = "./apps/"
-  environment = "${var.environment}"
-  instance_type = "${var.instance_type}"
-}
-
-module "platform" {
-  source = "./platform/"
-  environment = "${var.environment}"
-  instance_type = "${var.instance_type}"
-}
