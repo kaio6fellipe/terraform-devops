a74d9fccd18d58b42ffdc998af1a954f19d92e5a
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-07-28 03:56:43 -0300

    Ajuste na VPC

--- multi-region/vpc.tf
+++ multi-region/vpc.tf
@@ -25,42 +25,42 @@ module "vpc" {
 }
 
 #Delete the default vpc & its components when destroy is run, this will work only if the default igw is detached from the default vpc
-resource "aws_default_vpc" "default_vpc" {
-    force_destroy = true
-    tags = {
-        Name = "Default VPC"
-    }
-}
-
-resource "aws_default_subnet" "default_subnet_azs" {
-  for_each          = toset(data.aws_availability_zones.az_zones.names)
-  availability_zone = each.value
-  force_destroy     = true
-  # vpc_id            = aws_default_vpc.default_vpc.id
-  depends_on = [aws_default_vpc.default_vpc]
-}
-
-resource "aws_default_route_table" "default_rt" {
-  depends_on             = [aws_default_vpc.default_vpc]
-  default_route_table_id = aws_default_vpc.default_vpc.default_route_table_id
-
-  route = []
-
-  tags = {
-    Name = "default-route-table"
-  }
-}
-
-resource "aws_default_security_group" "default_sg" {
-  depends_on = [aws_default_vpc.default_vpc]
-  vpc_id     = aws_default_vpc.default_vpc.id
-}
-
-resource "aws_default_network_acl" "default_nacl" {
-  depends_on             = [aws_default_vpc.default_vpc]
-  default_network_acl_id = aws_default_vpc.default_vpc.default_network_acl_id
-}
-
-resource "aws_default_vpc_dhcp_options" "default_dhcp" {
-  depends_on = [aws_default_vpc.default_vpc]
-}
+# resource "aws_default_vpc" "default_vpc" {
+#     force_destroy = true
+#     tags = {
+#         Name = "Default VPC"
+#     }
+# }
+# 
+# resource "aws_default_subnet" "default_subnet_azs" {
+#   for_each          = toset(data.aws_availability_zones.az_zones.names)
+#   availability_zone = each.value
+#   force_destroy     = true
+#   # vpc_id            = aws_default_vpc.default_vpc.id
+#   depends_on = [aws_default_vpc.default_vpc]
+# }
+# 
+# resource "aws_default_route_table" "default_rt" {
+#   depends_on             = [aws_default_vpc.default_vpc]
+#   default_route_table_id = aws_default_vpc.default_vpc.default_route_table_id
+# 
+#   route = []
+# 
+#   tags = {
+#     Name = "default-route-table"
+#   }
+# }
+# 
+# resource "aws_default_security_group" "default_sg" {
+#   depends_on = [aws_default_vpc.default_vpc]
+#   vpc_id     = aws_default_vpc.default_vpc.id
+# }
+# 
+# resource "aws_default_network_acl" "default_nacl" {
+#   depends_on             = [aws_default_vpc.default_vpc]
+#   default_network_acl_id = aws_default_vpc.default_vpc.default_network_acl_id
+# }
+# 
+# resource "aws_default_vpc_dhcp_options" "default_dhcp" {
+#   depends_on = [aws_default_vpc.default_vpc]
+# }
