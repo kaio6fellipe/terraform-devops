bc7bc02f8e6d95da8ef212d74177541275144e1d
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-07 06:15:16 -0300

    Iniciando configurações adicionais na bastion para inclusao da chave privada via secrets no github

--- multi-region/platform/bastion/ec2.tf
+++ multi-region/platform/bastion/ec2.tf
@@ -13,6 +13,15 @@ module "ec2-instance" {
   subnet_id              = var.public_subnet_id_0
   vpc_security_group_ids = ["${aws_security_group.bastion_public_ssh.id}", "${var.allow_ansible_admin_subnet_ssh}", "${var.allow_outbound}", "${var.allow_ping}"]
 
+  user_data = <<EOF
+#!/bin/bash
+echo "Copying the SSH Key to Bastion server"
+
+echo "Changing Hostname"
+hostname "bastion01-${var.environment}"
+echo "bastion01-${var.environment}" > /etc/hostname
+EOF
+
   tags = {
     Type = var.instance_type
     App  = "bastion"
