44e50b6b92703cbc47720409160993d99d585db6
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-07 04:28:57 -0300

    Ajuste no CIDR block da SG com erro

--- multi-region/locals.tf
+++ multi-region/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  availability_zone_0 = element(module.vpc.azs, 0)
-  private_subnet_id_0 = element(module.vpc.private_subnets, 0)
-  public_subnet_id_0  = element(module.vpc.public_subnets, 0)
-  vpc_id              = module.vpc.vpc_id
+  availability_zone_0         = element(module.vpc.azs, 0)
+  private_subnet_id_0         = element(module.vpc.private_subnets, 0)
+  private_subnet_cidr_block_0 = element(module.vpc.private_subnets_cidr_blocks, 0)
+  public_subnet_id_0          = element(module.vpc.public_subnets, 0)
+  vpc_id                      = module.vpc.vpc_id
 }
--- multi-region/main.tf
+++ multi-region/main.tf
@@ -5,14 +5,15 @@ module "apps" {
 }
 
 module "platform" {
-  source                = "./platform"
-  instance_type         = var.instance_type
-  ansible_instance_type = var.ansible_instance_type
-  environment           = var.environment
-  ubuntu2004_id         = data.aws_ami.ubuntu2004.id
-  key_name              = "${var.key_name}-${var.environment}"
-  vpc_id                = local.vpc_id
-  availability_zone_0   = local.availability_zone_0
-  private_subnet_id_0   = local.private_subnet_id_0
-  public_subnet_id_0    = local.public_subnet_id_0
+  source                      = "./platform"
+  instance_type               = var.instance_type
+  ansible_instance_type       = var.ansible_instance_type
+  environment                 = var.environment
+  ubuntu2004_id               = data.aws_ami.ubuntu2004.id
+  key_name                    = "${var.key_name}-${var.environment}"
+  vpc_id                      = local.vpc_id
+  availability_zone_0         = local.availability_zone_0
+  private_subnet_id_0         = local.private_subnet_id_0
+  public_subnet_id_0          = local.public_subnet_id_0
+  private_subnet_cidr_block_0 = local.private_subnet_cidr_block_0
 }
