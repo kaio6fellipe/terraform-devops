62bca189190723247d2fc0a367130feac93cb59d
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-10 03:52:26 -0300

    Ajustando a escrita de logs e os scripts passados no user_data

--- multi-region/platform/ansible-server/ec2.tf
+++ multi-region/platform/ansible-server/ec2.tf
@@ -15,29 +15,29 @@ module "ec2-instance" {
 
   user_data = <<EOF
 #!/bin/bash
-sudo echo "Changing Hostname of Ansible Server" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Changing Hostname of Ansible Server" >> /var/log/terraform.log
 sudo hostname "ansible-server01-${var.environment}"
 sudo echo "ansible-server01-${var.environment}" > /etc/hostname
 
-sudo echo "Running updates with apt-get" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Running updates with apt-get" >> /var/log/terraform.log
 sudo apt-get update -y
 
-sudo echo "Copying the SSH Key to Ansible server" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Copying the SSH Key to Ansible server" >> /var/log/terraform.log
 sudo echo "${var.SSH_PRIVATE_KEY}" > /home/ubuntu/.ssh/"terraform-aws-${var.environment}"
 
-sudo echo "Setup Ansible Server" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Setup Ansible Server" >> /var/log/terraform.log
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ansible
-sudo echo "Ansible Setup Successfully" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Ansible Setup Successfully" >> /var/log/terraform.log
 sudo ansible --version >> /var/log/terraform.log
 
-sudo echo "Changing permissions of the SSH Key" >> /var/log/terraform.log
-sudo chmod 400 /home/ubuntu/.ssh/"terraform-aws-${var.environment}
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Changing permissions of the SSH Key" >> /var/log/terraform.log
+sudo chmod 400 /home/ubuntu/.ssh/"terraform-aws-${var.environment}"
 
-sudo echo "Rebooting" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Rebooting" >> /var/log/terraform.log
 sudo reboot
 EOF
 
--- multi-region/platform/bastion/ec2.tf
+++ multi-region/platform/bastion/ec2.tf
@@ -15,19 +15,19 @@ module "ec2-instance" {
 
   user_data = <<EOF
 #!/bin/bash
-sudo echo "Changing Hostname o Bastion Server" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Changing Hostname o Bastion Server" >> /var/log/terraform.log
 sudo hostname "bastion01-${var.environment}"
 sudo echo "bastion01-${var.environment}" > /etc/hostname
 
-sudo echo "Running updates with apt update and upgrade" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Running updates with apt update and upgrade" >> /var/log/terraform.log
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo echo "Copying the SSH Key to Bastion server" >> /var/log/terraform.log
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Copying the SSH Key to Bastion server" >> /var/log/terraform.log
 sudo echo "${var.SSH_PRIVATE_KEY}" > /home/ubuntu/.ssh/"terraform-aws-${var.environment}"
 
-sudo echo "Changing permissions of the SSH Key" >> /var/log/terraform.log
-sudo chmod 400 /home/ubuntu/.ssh/"terraform-aws-${var.environment}
+sudo echo "$(date '+%d%m%Y_%Hh%M') - Changing permissions of the SSH Key" >> /var/log/terraform.log
+sudo chmod 400 /home/ubuntu/.ssh/"terraform-aws-${var.environment}"
 EOF
 
   tags = {
