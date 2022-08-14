5fab0075c3e45c5c9e27a105c8a61e5426a41c64
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-13 23:36:05 -0300

    Inclusão de roles de acesso para a github actions do ansible-server

--- multi-region/platform/ansible-server/s3.tf
+++ multi-region/platform/ansible-server/s3.tf
@@ -1,8 +1,16 @@
 resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "codepipeline-ansible-ktech-br-${var.environment}"
-  acl    = "private"
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_acl" "codepipeline_bucket" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "codepipeline_bucket"{
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
