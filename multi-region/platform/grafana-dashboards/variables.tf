b418b2843b393e177dd19d4099f6045902c20e9b
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-19 04:56:35 -0300

    Inclusão das configurações iniciais para o Grafana-Dashboards

---
+++ multi-region/platform/grafana-dashboards/variables.tf
@@ -0,0 +1,31 @@
+variable "cdirs_acesso_remoto" {
+  default = ["201.33.195.0/24", "201.33.205.0/24"]
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "amazon_linux_2" {}
+
+variable "vpc_id" {}
+
+variable "availability_zone_0" {}
+
+variable "public_subnet_id_0" {}
+
+variable "allow_ansible_admin_subnet_ssh" {}
+
+variable "allow_outbound" {}
+
+variable "allow_ping" {}
+
+variable "allow_bastion_ssh" {}
--- multi-region/platform/main.tf
+++ multi-region/platform/main.tf
@@ -27,3 +27,18 @@ module "bastion" {
   allow_ping                     = aws_security_group.allow_ping.id
   SSH_PRIVATE_KEY                = var.SSH_PRIVATE_KEY
 }
+
+module "grafana_dashboards" {
+  source                         = "./grafana-dashboards"
+  instance_type                  = var.instance_type
+  environment                    = var.environment
+  amazon_linux_2                 = var.amazon_linux_2
+  key_name                       = var.key_name
+  vpc_id                         = var.vpc_id
+  availability_zone_0            = var.availability_zone_0
+  public_subnet_id_0             = var.public_subnet_id_0
+  allow_bastion_ssh              = aws_security_group.allow_bastion_ssh.id
+  allow_ansible_admin_subnet_ssh = aws_security_group.allow_ansible_admin_subnet_ssh.id
+  allow_outbound                 = aws_security_group.allow_outbound.id
+  allow_ping                     = aws_security_group.allow_ping.id
+}
