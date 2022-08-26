a52dc3a2e62f0a82281c5205bc77725f1dcd384e
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-26 04:52:44 -0300

    Inclusão do cidr block do meu provedor

--- multi-region/platform/grafana-dashboards/alb.tf
+++ multi-region/platform/grafana-dashboards/alb.tf
@@ -12,7 +12,7 @@ module "alb" {
 
   target_groups = [
     {
-      name_prefix      = "pref-"
+      name_prefix      = "alb-grafana-dashboards-${var.environment}"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
