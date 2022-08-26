caf5739700ecd88d0db5e2220497376aa06a1747
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-26 04:32:17 -0300

    Ajuste de bugs no terraform apply

--- multi-region/locals.tf
+++ multi-region/locals.tf
@@ -1,7 +1,10 @@
 locals {
   availability_zone_0         = element(module.vpc.azs, 0)
   private_subnet_id_0         = element(module.vpc.private_subnets, 0)
+  availability_zone_1         = element(module.vpc.azs, 1)
   private_subnet_id_1         = element(module.vpc.private_subnets, 1)
+  availability_zone_2         = element(module.vpc.azs, 2)
+  private_subnet_id_2         = element(module.vpc.private_subnets, 2)
   private_subnet_cidr_block_0 = element(module.vpc.private_subnets_cidr_blocks, 0)
   public_subnet_id_0          = element(module.vpc.public_subnets, 0)
   database_subnet_id_0        = element(module.vpc.database_subnets, 0)
