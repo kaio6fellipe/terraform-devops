8f7f35a034d6208a89fd0f08b8c9643720737de3
kaio-fellipe <kaio6fellipe@gmail.com>
2022-10-13 21:56:20 -0300

    Grafana public CNAME change to use wildcard certificate

--- multi-region/platform/grafana-dashboards/route53.tf
+++ multi-region/platform/grafana-dashboards/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "grafana_public" {
   zone_id = var.public_dns_zone_id
-  name = "grafana.${var.environment}.${var.public_dns_zone_name}"
+  name = "grafana-${var.environment}.${var.public_dns_zone_name}"
   type = "CNAME"
   ttl = "300"
   records = [module.alb.lb_dns_name]
