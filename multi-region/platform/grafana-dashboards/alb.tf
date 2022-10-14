271377c324320110e5f601ad30415bbb8b593965
kaio-fellipe <kaio6fellipe@gmail.com>
2022-10-13 21:27:52 -0300

    HTTPS support for Grafana public load balancer, closes #98

--- multi-region/platform/grafana-dashboards/alb.tf
+++ multi-region/platform/grafana-dashboards/alb.tf
@@ -6,9 +6,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = var.vpc_id
-  subnets            = [var.public_subnet_id_1, var.public_subnet_id_2]
-  security_groups    = [aws_security_group.sg_grafana_http.id, aws_security_group.sg_grafana_https.id, var.allow_outbound, var.allow_ping]
+  vpc_id          = var.vpc_id
+  subnets         = [var.public_subnet_id_1, var.public_subnet_id_2]
+  security_groups = [aws_security_group.sg_grafana_http.id, aws_security_group.sg_grafana_https.id, var.allow_outbound, var.allow_ping]
 
   target_groups = [
     {
@@ -30,26 +30,31 @@ module "alb" {
       targets = {
         target = {
           target_id = "${module.ec2-instance.id}"
-          port = 3000
+          port      = 3000
         }
       }
     }
   ]
 
-  # https_listeners = [
-  #   {
-  #     port               = 443
-  #     protocol           = "HTTPS"
-  #     certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-  #     target_group_index = 0
-  #   }
-  # ]
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = local.wildcard_certificate
+      target_group_index = 0
+    }
+  ]
 
   http_tcp_listeners = [
     {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
   ]
 
