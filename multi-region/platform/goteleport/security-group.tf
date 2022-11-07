61588ad132dfd677ed125d8e1cf9e19f68a11773
kaio-fellipe <kaio6fellipe@gmail.com>
2022-11-07 01:36:26 -0300

    Including sg to allow https connections in teleport server, closes #108

--- multi-region/platform/goteleport/security-group.tf
+++ multi-region/platform/goteleport/security-group.tf
@@ -1,15 +1,15 @@
-resource "aws_security_group" "goteleport_public_ssh" {
-  name        = "goteleport_public_ssh-${var.environment}"
-  description = "goteleport_public_ssh-${var.environment}"
+resource "aws_security_group" "goteleport_public_https" {
+  name        = "goteleport_public_https-${var.environment}"
+  description = "goteleport_public_https-${var.environment}"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = var.cdirs_acesso_remoto
   }
   tags = {
-    Name = "ssh"
+    Name = "http/https"
   }
 }
