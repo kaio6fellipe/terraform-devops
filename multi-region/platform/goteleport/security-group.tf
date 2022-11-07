e8e0816f7da4721568140dfb2157ccb2f146daf2
Kaio Fellipe <kaiofellipe_@hotmail.com>
2022-11-07 03:05:03 -0300

    Update security-group.tf

--- multi-region/platform/goteleport/security-group.tf
+++ multi-region/platform/goteleport/security-group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "goteleport_public_https" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    cidr_blocks = var.cdirs_acesso_remoto
+    # cidr_blocks = var.cdirs_acesso_remoto
   }
   tags = {
     Name = "https"
