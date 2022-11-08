488777201d53ed3ea7f12cd7bd3fe9abe62c3107
kaio-fellipe <kaio6fellipe@gmail.com>
2022-11-08 20:49:36 -0300

    Remove aws_security_group_rule of default rules

--- multi-region/platform/ansible-security-group.tf
+++ multi-region/platform/ansible-security-group.tf
@@ -4,37 +4,31 @@ resource "aws_security_group" "default_ansible" {
   description = "Default SG for Ansible env: ${var.environment}"
   vpc_id      = var.vpc_id
 
+  egress {
+    description = "Permit all outbound traffic in env: ${var.environment}"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Permit inbound ping in env: ${var.environment}"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  ingress {
+    description     = "Permit inbound ssh from bastion host in env: ${var.environment}"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [local.bastion_sg_id]
+  }
+
   tags = {
     Name = "default_ansible_sg"
   }
-}
-
-resource "aws_security_group_rule" "default_ansible_egress" {
-  description       = "Permit all outbound traffic in env: ${var.environment}"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.default_ansible.id
-}
-
-resource "aws_security_group_rule" "allow_ansible_ping" {
-  description       = "Permit inbound ping in env: ${var.environment}"
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = ["${var.vpc_cidr}"]
-  security_group_id = aws_security_group.default_ansible.id
-}
-
-resource "aws_security_group_rule" "allow_bastion_ansible_ssh" {
-  description              = "Permit inbound ssh from bastion host in env: ${var.environment}"
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  source_security_group_id = local.bastion_sg_id
-  security_group_id        = aws_security_group.default_ansible.id
 }
--- multi-region/platform/bastion-security-group.tf
+++ multi-region/platform/bastion-security-group.tf
@@ -4,37 +4,31 @@ resource "aws_security_group" "default_bastion" {
   description = "Default SG for Bastion in env: ${var.environment}"
   vpc_id      = var.vpc_id
 
+  egress {
+    description = "Permit all outbound traffic in env: ${var.environment}"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Permit inbound ping in env: ${var.environment}"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  ingress {
+    description     = "Permit inbound ssh from ansible host in env: ${var.environment}"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [local.ansible_sg_id]
+  }
+
   tags = {
     Name = "default_bastion_sg"
   }
-}
-
-resource "aws_security_group_rule" "default_bastion_egress" {
-  description       = "Permit all outbound traffic in env: ${var.environment}"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "allow_bastion_ping" {
-  description       = "Permit inbound ping in env: ${var.environment}"
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = ["${var.vpc_cidr}"]
-  security_group_id = aws_security_group.default.id
-}
-
-resource "aws_security_group_rule" "allow_ansible_bastion_ssh" {
-  description              = "Permit inbound ssh from ansible host in env: ${var.environment}"
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  source_security_group_id = local.ansible_sg_id
-  security_group_id        = aws_security_group.default.id
 }
