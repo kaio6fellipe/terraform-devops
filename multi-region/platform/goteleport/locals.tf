f31e1366d2fbc43107605fff5d65c1a1a87fb497
kaio-fellipe <kaio6fellipe@gmail.com>
2022-11-09 03:00:31 -0300

    Altering ingress_cidr_block of teleport server

--- multi-region/platform/goteleport/locals.tf
+++ multi-region/platform/goteleport/locals.tf
@@ -3,6 +3,6 @@ locals {
   route53_domain                  = "teleport-${var.environment}.${var.route53_zone}"
   s3_bucket_name                  = "teleport-certificates-ktech-br-${var.environment}"
   allowed_ssh_ingress_cidr_blocks = var.cdirs_acesso_remoto
-  allowed_ingress_cidr_blocks     = var.cdirs_acesso_remoto
+  allowed_ingress_cidr_blocks     = ["0.0.0.0/0"]
   allowed_egress_cidr_blocks      = ["0.0.0.0/0"]
 }
