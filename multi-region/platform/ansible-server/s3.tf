c0a5820abf85aa851ae02ff25c61c094d6a8b6e7
kaio-fellipe <kaio6fellipe@gmail.com>
2022-11-18 01:14:52 -0300

    Change server_side encryption of ansible codepipeline_bucket resource

--- multi-region/platform/ansible-server/s3.tf
+++ multi-region/platform/ansible-server/s3.tf
@@ -1,20 +1,33 @@
 resource "aws_s3_bucket" "codepipeline_bucket" {
   #checkov:skip=CKV_AWS_144: For demo purposes, don't need cross-region replication enabled
+  #checkov:skip=CKV_AWS_145: For demo purposes, don't need default encryption with KMS
   bucket        = "codepipeline-ansible-ktech-br-${var.environment}"
   force_destroy = true
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.ansible_bucket_key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       kms_master_key_id = aws_kms_key.ansible_bucket_key.arn
+  #       sse_algorithm     = "aws:kms"
+  #     }
+  #   }
+  # }
 
   logging {
     target_bucket = "codepipeline-ansible-ktech-br-${var.environment}"
     target_prefix = "bucket-access-log/codepipeline-ansible-ktech-br-${var.environment}"
+  }
+}
+
+// For demo purposes, CMK is not needed
+// tfsec:ignore:aws-s3-encryption-customer-key
+resource "aws_s3_bucket_server_side_encryption_configuration" "codepipeline_bucket" {
+  bucket = aws_s3_bucket.codepipeline_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
