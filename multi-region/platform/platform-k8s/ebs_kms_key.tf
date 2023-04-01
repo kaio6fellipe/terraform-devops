0227fe40c9db25889690a95563dce65f4873f895
kaio-fellipe <kaio6fellipe@gmail.com>
2023-04-01 03:18:21 -0300

    Change key administrators of ebs node groups

--- multi-region/platform/platform-k8s/ebs_kms_key.tf
+++ multi-region/platform/platform-k8s/ebs_kms_key.tf
@@ -5,9 +5,9 @@ module "ebs_kms_key" {
   description = "Customer managed key to encrypt EKS managed node group volumes"
 
   # Policy
-  key_administrators = [
-    local.current_identity
-  ]
+  # key_administrators = [
+  #   local.current_identity
+  # ]
 
   key_service_roles_for_autoscaling = [
     # required for the ASG to manage encrypted volumes for nodes
