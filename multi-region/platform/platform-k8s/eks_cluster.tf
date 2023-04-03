a44f383f7b163f0db0799fa900ae6223f4aac176
kaio-fellipe <kaio6fellipe@gmail.com>
2023-04-03 02:38:04 -0300

    Removing default node_group

--- multi-region/platform/platform-k8s/eks_cluster.tf
+++ multi-region/platform/platform-k8s/eks_cluster.tf
@@ -78,19 +78,19 @@ module "eks" {
 
   eks_managed_node_groups = {
     # Default node group - as provided by AWS EKS
-    default_node_group = {
-      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
-      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
-      use_custom_launch_template = false
-
-      disk_size = 50
-
-      # Remote access cannot be specified with a launch template
-      remote_access = {
-        ec2_ssh_key               = var.key_name
-        source_security_group_ids = [var.default_sg]
-      }
-    }
+    # default_node_group = {
+    #   # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
+    #   # so we need to disable it to use the default template provided by the AWS EKS managed node group service
+    #   use_custom_launch_template = false
+# 
+    #   disk_size = 50
+# 
+    #   # Remote access cannot be specified with a launch template
+    #   remote_access = {
+    #     ec2_ssh_key               = var.key_name
+    #     source_security_group_ids = [var.default_sg]
+    #   }
+    # }
 
     # Complete
     complete = {
