b3f187f8b5a427dda6c863961609201452a1dcc3
kaio-fellipe <kaio6fellipe@gmail.com>
2023-04-11 01:58:48 -0300

    Include Helm Release for ArgoCD

--- stack/platform/platform-k8s/providers.tf
+++ stack/platform/platform-k8s/providers.tf
@@ -17,7 +17,15 @@
 # }
 
 provider "kubernetes" {
-  host                   = try(data.external.aws_eks_cluster_endpoint.result.cluster_endpoint,"")
-  cluster_ca_certificate = try(base64decode(data.external.aws_eks_cluster_ca_certificate.result.cluster_ca_certificate),"")
-  token                  = try(data.external.aws_eks_cluster_token.result.cluster_token,"")
+  host                   = try(data.external.aws_eks_cluster_endpoint.result.cluster_endpoint, "")
+  cluster_ca_certificate = try(base64decode(data.external.aws_eks_cluster_ca_certificate.result.cluster_ca_certificate), "")
+  token                  = try(data.external.aws_eks_cluster_token.result.cluster_token, "")
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = try(data.external.aws_eks_cluster_endpoint.result.cluster_endpoint, "")
+    cluster_ca_certificate = try(base64decode(data.external.aws_eks_cluster_ca_certificate.result.cluster_ca_certificate), "")
+    token                  = try(data.external.aws_eks_cluster_token.result.cluster_token, "")
+  }
 }
