b2552c9c918434e5ad809180e08344c64562e87a
kaio-fellipe <kaio6fellipe@gmail.com>
2023-04-29 02:34:01 -0300

    [skip ci] Remove Grafana Dashboards instances to reduce cost

--- stack/platform/main.tf
+++ stack/platform/main.tf
@@ -51,33 +51,33 @@ module "bastion" {
 #   # allow_bastion_ssh              = aws_security_group.allow_bastion_ssh.id
 # }
 
-module "grafana_dashboards" {
-  source                    = "./grafana-dashboards"
-  instance_type             = var.instance_type
-  db_instance_type          = var.db_instance_type
-  rds_allocated_storage     = var.rds_allocated_storage
-  rds_max_allocated_storage = var.rds_max_allocated_storage
-  environment               = var.environment
-  amazon_linux_2            = var.amazon_linux_2
-  key_name                  = var.key_name
-  vpc_id                    = var.vpc_id
-  vpc_cidr                  = var.vpc_cidr
-  availability_zone_1       = var.availability_zone_1
-  private_subnet_id_1       = var.private_subnet_id_1
-  availability_zone_2       = var.availability_zone_2
-  private_subnet_id_2       = var.private_subnet_id_2
-  public_subnet_id_1        = var.public_subnet_id_1
-  public_subnet_id_2        = var.public_subnet_id_2
-  default_sg                = aws_security_group.default_platform.id
-  cdirs_acesso_remoto       = var.cdirs_acesso_remoto
-  database_subnet_id_0      = var.database_subnet_id_0
-  db_subnet_group_name      = var.db_subnet_group_name
-  private_dns_zone_id       = var.private_dns_zone_id
-  private_dns_zone_name     = var.private_dns_zone_name
-  public_dns_zone_name      = data.aws_route53_zone.ktech_public.name
-  public_dns_zone_id        = data.aws_route53_zone.ktech_public.zone_id
-  AWS_RDS_PASSWORD          = var.AWS_RDS_PASSWORD
-}
+# module "grafana_dashboards" {
+#   source                    = "./grafana-dashboards"
+#   instance_type             = var.instance_type
+#   db_instance_type          = var.db_instance_type
+#   rds_allocated_storage     = var.rds_allocated_storage
+#   rds_max_allocated_storage = var.rds_max_allocated_storage
+#   environment               = var.environment
+#   amazon_linux_2            = var.amazon_linux_2
+#   key_name                  = var.key_name
+#   vpc_id                    = var.vpc_id
+#   vpc_cidr                  = var.vpc_cidr
+#   availability_zone_1       = var.availability_zone_1
+#   private_subnet_id_1       = var.private_subnet_id_1
+#   availability_zone_2       = var.availability_zone_2
+#   private_subnet_id_2       = var.private_subnet_id_2
+#   public_subnet_id_1        = var.public_subnet_id_1
+#   public_subnet_id_2        = var.public_subnet_id_2
+#   default_sg                = aws_security_group.default_platform.id
+#   cdirs_acesso_remoto       = var.cdirs_acesso_remoto
+#   database_subnet_id_0      = var.database_subnet_id_0
+#   db_subnet_group_name      = var.db_subnet_group_name
+#   private_dns_zone_id       = var.private_dns_zone_id
+#   private_dns_zone_name     = var.private_dns_zone_name
+#   public_dns_zone_name      = data.aws_route53_zone.ktech_public.name
+#   public_dns_zone_id        = data.aws_route53_zone.ktech_public.zone_id
+#   AWS_RDS_PASSWORD          = var.AWS_RDS_PASSWORD
+# }
 
 # module "grafana_loki" {
 #   source                = "./grafana-loki"
