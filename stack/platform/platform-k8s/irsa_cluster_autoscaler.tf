2f221287be5db71793720ad87cd93adbde3f6dc1
kaio-fellipe <kaio6fellipe@gmail.com>
2023-05-02 18:35:48 -0300

    Adjusting typo on autoscaler clusters list

--- stack/platform/platform-k8s/irsa_cluster_autoscaler.tf
+++ stack/platform/platform-k8s/irsa_cluster_autoscaler.tf
@@ -1,10 +1,10 @@
 module "cluster_autoscaler_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.17.0"
+  version = "~> 5.0"
 
   role_name                        = "cluster-autoscaler-${var.environment}"
   attach_cluster_autoscaler_policy = true
-  cluster_autoscaler_cluster_ids   = [module.eks.cluster_id]
+  cluster_autoscaler_cluster_ids   = [module.eks.cluster_name]
 
   oidc_providers = {
     ex = {
