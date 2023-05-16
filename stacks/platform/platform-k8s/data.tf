79f5be4994a3e75a68f842347fa5c6f9b737054d
kaio-fellipe <kaio6fellipe@gmail.com>
2023-05-16 18:44:44 -0300

    Debug Crossplane with IRSA

--- stacks/platform/platform-k8s/data.tf
+++ stacks/platform/platform-k8s/data.tf
@@ -69,9 +69,3 @@ data "github_repository_file" "argocd_apps" {
   branch     = "main"
   file       = "config/argocd-apps-values.yaml"
 }
-
-data "github_repository_file" "crossplane" {
-  repository = "kaio6fellipe/argo"
-  branch     = "main"
-  file       = "clusters/platform-eks-dev/crossplane/crossplane-values.yaml"
-}
