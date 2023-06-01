f8d9fd04c1c418503a9320e679ff4fcd2f254a25
kaio6fellipe <kaio6fellipe@gmail.com>
2023-06-01 00:53:31 -0300

    Change EBS-CSI role name

--- stacks/aws/platform/platform-k8s/irsa_ebs_csi.tf
+++ stacks/aws/platform/platform-k8s/irsa_ebs_csi.tf
@@ -2,7 +2,7 @@ module "ebs_csi_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.0"
 
-  role_name_prefix      = "EBS-CSI-IRSA"
+  role_name             = "EBS-CSI-IRSA"
   attach_ebs_csi_policy = true
 
   oidc_providers = {
