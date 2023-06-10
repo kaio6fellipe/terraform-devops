9342ae9dae40719f3dd68184da5353d111b69879
kaio6fellipe <kaio6fellipe@gmail.com>
2023-06-10 13:12:40 -0300

    Adjust Grafana Cloud Terraform managed dashboards and folders

--- stacks/grafana-cloud/dashboards.tf
+++ stacks/grafana-cloud/dashboards.tf
@@ -4,34 +4,22 @@ resource "grafana_dashboard" "argocd-overview" {
   message     = "Changed by Terraform"
   config_json = file("dashboards/argocd-overview.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "grafana_dashboard" "github-details" {
   provider    = grafana.ktechdevops
-  folder      = grafana_folder.github.uid
+  folder      = grafana_folder.integration-github.uid
   message     = "Changed by Terraform"
   config_json = file("dashboards/github-details.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "grafana_dashboard" "github-overview" {
   provider    = grafana.ktechdevops
-  folder      = grafana_folder.github.uid
+  folder      = grafana_folder.integration-github.uid
   message     = "Changed by Terraform"
   config_json = file("dashboards/github-overview.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "grafana_dashboard" "loki-dashboard" {
@@ -40,21 +28,14 @@ resource "grafana_dashboard" "loki-dashboard" {
   message     = "Changed by Terraform"
   config_json = file("dashboards/loki-dashboard.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "grafana_dashboard" "node-exporter-full" {
   provider    = grafana.ktechdevops
+  folder      = grafana_folder.integration-linux-node.uid
   message     = "Changed by Terraform"
   config_json = file("dashboards/node-exporter-full.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "grafana_dashboard" "red" {
@@ -62,8 +43,4 @@ resource "grafana_dashboard" "red" {
   message     = "Changed by Terraform"
   config_json = file("dashboards/red.json")
   overwrite   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
--- stacks/grafana-cloud/folders.tf
+++ stacks/grafana-cloud/folders.tf
@@ -1,7 +1,7 @@
-resource "grafana_folder" "github" {
+resource "grafana_folder" "integration-github" {
   provider = grafana.ktechdevops
-  uid      = "github"
-  title    = "GitHub"
+  uid      = "integration---github"
+  title    = "Integration - Github (Terraform)"
 
   lifecycle {
     prevent_destroy = true
@@ -11,7 +11,7 @@ resource "grafana_folder" "github" {
 resource "grafana_folder" "argocd" {
   provider = grafana.ktechdevops
   uid      = "argocd"
-  title    = "ArgoCD"
+  title    = "ArgoCD (Terraform)"
 
   lifecycle {
     prevent_destroy = true
@@ -21,17 +21,27 @@ resource "grafana_folder" "argocd" {
 resource "grafana_folder" "loki" {
   provider = grafana.ktechdevops
   uid      = "loki"
-  title    = "Loki"
+  title    = "Loki (Terraform)"
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
-resource "grafana_folder" "aws" {
+resource "grafana_folder" "integration-cloudwatch-metrics" {
   provider = grafana.ktechdevops
-  uid      = "aws"
-  title    = "AWS"
+  uid      = "integration---cloudwatch-metrics"
+  title    = "Integration - CloudWatch Metrics (Terraform)"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "grafana_folder" "integration-linux-node" {
+  provider = grafana.ktechdevops
+  uid      = "integration---linux-node"
+  title   = "Integration - Linux Node (Terraform)"
 
   lifecycle {
     prevent_destroy = true
