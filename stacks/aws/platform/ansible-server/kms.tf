d818e0ce08151ce0ac193b23e6e9767c63cc277b
kaio6fellipe <kaio6fellipe@gmail.com>
2023-06-22 21:38:01 -0300

    [skip ci] Adjust checkov report for ansible_bucket_key on ansible-server stack

--- stacks/aws/platform/ansible-server/kms.tf
+++ stacks/aws/platform/ansible-server/kms.tf
@@ -1,4 +1,5 @@
 resource "aws_kms_key" "ansible_bucket_key" {
+  #checkov:skip=CKV2_AWS_64: For demo purposes, dont need key policy definition
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
   enable_key_rotation     = true
